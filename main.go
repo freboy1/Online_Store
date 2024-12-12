a620f5e71214a9b9852ddd0cf926aa5055bb46e8
freboy1 <dautovalisher33@gmail.com>
2024-12-12 19:10:22 +0500

    Sending and Parsing JSON Responses/Requests GET && POST

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+type GetMessage struct {
+    Message	string
+}
+
+type PostMessage struct {
+	Status	string
+	Message string
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		getMessage := GetMessage{
+			Message: "Hello, server! This is JSON data from Postman.",
+		}
+		jsonGetMessage, err := json.Marshal(getMessage)
+		if err != nil {
+			getMessage.Message = "Invalid JSON message"
+			jsonGetMessage, _ = json.Marshal(getMessage)
+		}
+		w.Write(jsonGetMessage)
+
+	} else if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		postValue := r.FormValue("message")
+		postMessage := PostMessage {
+			Status: "fail",
+			Message: "Invalid JSON message",
+		}
+		if postValue != "" {
+			_, err := strconv.Atoi(postValue)
+			if err != nil {
+				postMessage.Status = "success"
+				postMessage.Message ="Data successfully received"
+			}
+		}
+		jsonGetMessage, _ := json.Marshal(postMessage)
+		w.Write(jsonGetMessage)
+	}
+}
+
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
