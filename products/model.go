b9cbb160d9b677837588cacc23300ffc3b8335a5
freboy1 <dautovalisher33@gmail.com>
2024-12-16 00:34:30 +0500

    Update ProductModel

--- products/model.go
+++ products/model.go
@@ -1,9 +1,10 @@
 package products
 
 type ProductModel struct {
-    Name        string `bson:"name"`
-    Description string `bson:"description"`
-    Price       int    `bson:"price"`
-    Discount    int    `bson:"discount"`
-    Quantity    int    `bson:"quantity"`
+	ID          int    `bson:"id"`
+	Name        string `bson:"name"`
+	Description string `bson:"description"`
+	Price       int    `bson:"price"`
+	Discount    int    `bson:"discount"`
+	Quantity    int    `bson:"quantity"`
 }
