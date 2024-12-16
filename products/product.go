21d55fa4fc77ef1b3586afdbd0432382dd7d2200
freboy1 <dautovalisher33@gmail.com>
2024-12-16 15:20:20 +0500

    Finish get product

--- products/product.go
+++ products/product.go
@@ -1,21 +1,25 @@
 package products
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"html/template"
 )
 
 func Product(w http.ResponseWriter, r *http.Request, client *mongo.Client, database, collection string) {
 	vars := mux.Vars(r)
 	idStr := vars["id"] 
 	id, _ := strconv.ParseInt(idStr, 10, 64)
-	fmt.Println(id)
 	if r.Method == http.MethodGet {
 		product := GetProducts(client, database, collection, bson.D{{"id", id}})
-		fmt.Println(product)
+		tmpl, err := template.ParseFiles("templates/product.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, product)
 	}
 }
