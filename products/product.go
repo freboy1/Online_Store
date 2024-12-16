e5a4c0891e87e7115a1405bd1c5653bcf9d9adb9
freboy1 <dautovalisher33@gmail.com>
2024-12-16 15:33:05 +0500

    Finish Delete

--- products/product.go
+++ products/product.go
@@ -7,6 +7,9 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"html/template"
+	"fmt"
+	"context"
+	"log"
 )
 
 func Product(w http.ResponseWriter, r *http.Request, client *mongo.Client, database, collection string) {
@@ -21,5 +24,22 @@ func Product(w http.ResponseWriter, r *http.Request, client *mongo.Client, datab
 			return
 		}
 		tmpl.Execute(w, product)
+	} else if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		action := r.FormValue("action")
+		switch action {
+		case "delete":
+			result, err := deleteOne(client, context.TODO(), database, collection, id)
+			if err != nil {
+				log.Fatal(err)
+			}
+		
+			fmt.Println("Deleted succesfully:", result)
+			http.Redirect(w, r, "http://127.0.0.1:8080/products", http.StatusSeeOther)
+			return
+		}
 	}
 }
