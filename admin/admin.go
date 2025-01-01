b4f48b598387335016f188db68c9f75586f13e23
freboy1 <dautovalisher33@gmail.com>
2025-01-01 19:09:28 +0500

    Add render admin page

--- admin/admin.go
+++ admin/admin.go
@@ -2,12 +2,16 @@ package admin
 
 import (
 	"net/http"
+	"html/template"
 )
 
 func AdminPanelHandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method == http.MethodGet {
+		tmpl, _ := template.ParseFiles("templates/admin.html")
+		tmpl.Execute(w, map[string]interface{}{})
+	}
 }
 
 func SendEmailHandler(w http.ResponseWriter, r *http.Request) {
-
+	
 }
