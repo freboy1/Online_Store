c0dba1ad36194b77c570569d9c005a8646114363
freboy1 <dautovalisher33@gmail.com>
2025-01-01 19:45:56 +0500

    Fix name of package

--- main.go
+++ main.go
@@ -100,8 +100,8 @@ func main() {
 
 	admin.RegisterRoutes(mux, client, database, "Users")
 
-	mux.HandleFunc("/login", login.LoginHandler).Methods("POST")
-	mux.HandleFunc("/protected", login.ProtectedHandler).Methods("GET")
+	mux.HandleFunc("/login", auth.LoginHandler).Methods("POST")
+	mux.HandleFunc("/protected", auth.ProtectedHandler).Methods("GET")
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	err = http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
